493acec959161dd60a9ae6bbfe146a3d71b49e76
Sam Oehlert <sam.oehlert@gmail.com>
2024-12-17 01:10:08 -0600

    fix(import): use correct config file for settings

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 
 from fastapi import FastAPI
-from config import settings
+from backend.config import settings
 from backend.routers import music
 from backend.websockets import router as websockets_router
 from backend.websockets import update_websocket_clients
