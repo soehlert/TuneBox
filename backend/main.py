1efa667db1a9aca25697930d8ea4005ceaad6478
Sam Oehlert <sam.oehlert@gmail.com>
2024-12-26 12:58:00 -0600

    style(ruff): linter

--- backend/main.py
+++ backend/main.py
@@ -35,11 +35,17 @@ app.include_router(websockets_router)
 @app.on_event("startup")
 async def start_background_tasks():
     """Start background tasks for periodic updates."""
-    asyncio.create_task(update_websocket_clients())
+    # We need to assign the async tasks to something otherwise it could get garbage collected, thought that upsets ruff
+    # ruff: noqa: F841
+    tasks = await asyncio.create_task(update_websocket_clients())
 
 
 # Root endpoint
 @app.get("/", tags=["Root"])
 def read_root():
-    """Set up our base path."""
+    """Set up our base path.
+
+    Returns:
+        A JSON message so you know it worked.
+    """
     return {"message": "Welcome to the Tunebox API!"}
